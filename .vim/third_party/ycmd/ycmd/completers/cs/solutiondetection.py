381d5cd5b5baf97745c73f0499ae7e9256af3daf
Milan Nikolic <gen2brain@gmail.com>
2016-09-15 14:56:55 +0200

    update plugins

--- .vim/third_party/ycmd/ycmd/completers/cs/solutiondetection.py
+++ .vim/third_party/ycmd/ycmd/completers/cs/solutiondetection.py
@@ -1,15 +1,42 @@
+# Copyright (C) 2013  Google Inc.
+# Copyright (C) 2016  ycmd contributors.
+#
+# This file is part of ycmd.
+#
+# ycmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ycmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *  # noqa
 
 import os
 import glob
 import logging
 from inspect import getfile
 from ycmd import extra_conf_store
-
+from ycmd.utils import ToUnicode
 
 __logger = logging.getLogger( __name__ )
 
+
 def FindSolutionPath( filepath ):
-    """ Try to find suitable solution file given a source file path using all available information sources """
+    """Try to find suitable solution file given a source file path using all
+       available information sources"""
     # try to load ycm_extra_conf
     # if it needs to be verified, abort here and try again later
     module = extra_conf_store.ModuleForSourceFile( filepath )
@@ -21,15 +48,17 @@ def FindSolutionPath( filepath ):
 
     return path_to_solutionfile
 
+
 def PollModule( module, filepath ):
-  """ Try to use passed module in the selection process by calling CSharpSolutionFile on it """
+  """ Try to use passed module in the selection process by calling
+  CSharpSolutionFile on it """
   path_to_solutionfile = None
   module_hint = None
   if module:
     try:
       module_hint = module.CSharpSolutionFile( filepath )
-      __logger.info( 'extra_conf_store suggests {0} as solution file'.format(
-          str( module_hint ) ) )
+      __logger.info( u'extra_conf_store suggests {0} as solution file'.format(
+        ToUnicode( module_hint ) ) )
       if module_hint:
         # received a full path or one relative to the config's location?
         candidates = [ module_hint,
@@ -41,26 +70,28 @@ def PollModule( module, filepath ):
             # path seems to point to a solution
             path_to_solutionfile = path
             __logger.info(
-                'Using solution file {0} selected by extra_conf_store'.format(
-                path_to_solutionfile ) )
+                u'Using solution file {0} selected by extra_conf_store'.format(
+                  path_to_solutionfile ) )
             break
-    except AttributeError, e:
+    except AttributeError as e:
       # the config script might not provide solution file locations
       __logger.error(
-          'Could not retrieve solution for {0} from extra_conf_store: {1}'.format(
-          filepath, str( e ) ) )
+          u'Could not retrieve solution for {0}'
+           'from extra_conf_store: {1}'.format( filepath, str( e ) ) )
   return path_to_solutionfile
+
 
 def GuessFile( filepath ):
   """ Find solution files by searching upwards in the file tree """
   tokens = _PathComponents( filepath )
   for i in reversed( range( len( tokens ) - 1 ) ):
     path = os.path.join( *tokens[ : i + 1 ] )
-    candidates = glob.glob1( path, "*.sln" )
+    candidates = glob.glob1( path, '*.sln' )
     if len( candidates ) > 0:
       # do the whole procedure only for the first solution file(s) you find
       return _SolutionTestCheckHeuristics( candidates, tokens, i )
   return None
+
 
 def _SolutionTestCheckHeuristics( candidates, tokens, i ):
   """ Test if one of the candidate files stands out """
@@ -70,27 +101,33 @@ def _SolutionTestCheckHeuristics( candidates, tokens, i ):
   if len( candidates ) == 1 :
     selection = os.path.join( path, candidates[ 0 ] )
     __logger.info(
-        'Selected solution file {0} as it is the first one found'.format(
-        selection ) )
+        u'Selected solution file {0} as it is the first one found'.format(
+          selection ) )
+
   # there is more than one file, try some hints to decide
   # 1. is there a solution named just like the subdirectory with the source?
   if ( not selection and i < len( tokens ) - 1 and
-      "{0}.sln".format( tokens[ i + 1 ] ) in candidates ) :
-    selection = os.path.join( path, "{0}.sln".format( tokens[ i + 1 ] ) )
+       u'{0}.sln'.format( tokens[ i + 1 ] ) in candidates ):
+    selection = os.path.join( path, u'{0}.sln'.format( tokens[ i + 1 ] ) )
     __logger.info(
-        'Selected solution file {0} as it matches source subfolder'.format(
-        selection ) )
-  # 2. is there a solution named just like the directory containing the solution?
-  if not selection and "{0}.sln".format( tokens[ i ] ) in candidates :
-    selection = os.path.join( path, "{0}.sln".format( tokens[ i ] ) )
+        u'Selected solution file {0} as it matches source subfolder'.format(
+          selection ) )
+
+  # 2. is there a solution named just like the directory containing the
+  # solution?
+  if not selection and u'{0}.sln'.format( tokens[ i ] ) in candidates :
+    selection = os.path.join( path, u'{0}.sln'.format( tokens[ i ] ) )
     __logger.info(
-        'Selected solution file {0} as it matches containing folder'.format(
-        selection ) )
+        u'Selected solution file {0} as it matches containing folder'.format(
+          selection ) )
+
   if not selection:
     __logger.error(
-        'Could not decide between multiple solution files:\n{0}'.format(
-        candidates ) )
+        u'Could not decide between multiple solution files:\n{0}'.format(
+          candidates ) )
+
   return selection
+
 
 def _PathComponents( path ):
   path_components = []
@@ -104,5 +141,3 @@ def _PathComponents( path ):
       break
   path_components.reverse()
   return path_components
-
-
